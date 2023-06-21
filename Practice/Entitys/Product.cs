ed3be8a974fd93e61f8834b0b2642ef9b7fb4624
theMortysss <email>
2023-06-21 12:34:39 +0400

    Папочки

---
+++ Practice/Entitys/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice.Entitys;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public int? PackingG { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public DateTime? ExpirationDate { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();
+
+    public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+
+    public virtual ICollection<Supplie> Supplies { get; set; } = new List<Supplie>();
+}
---
+++ Practice/Entitys/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice.Entitys;
+
+public partial class Sale
+{
+    public int Id { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public DateTime? DateOfSale { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
