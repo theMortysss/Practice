2c82f823aeef042ea8e0c8da173ad5ceac86c5d7
theMortysss <email>
2023-06-21 11:34:48 +0400

    Редактирование интерфейса

---
+++ Practice/OrderForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Practice
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView2 = new DataGridView();
+            button1 = new Button();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Dock = DockStyle.Fill;
+            dataGridView2.Location = new Point(300, 0);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.RowTemplate.Height = 29;
+            dataGridView2.Size = new Size(500, 450);
+            dataGridView2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(54, 123);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Оформить заказ";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 450);
+            panel1.TabIndex = 4;
+            // 
+            // OrderForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView2);
+            Controls.Add(panel1);
+            Name = "OrderForm";
+            Text = "OrderForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView2;
+        private Button button1;
+        private Panel panel1;
+    }
+}
