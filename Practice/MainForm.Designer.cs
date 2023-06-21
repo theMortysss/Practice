e18ac92c076421c85001c93f6230b2290e734ed1
theMortysss <email>
2023-06-21 12:18:41 +0400

    Фикс

--- Practice/MainForm.Designer.cs
+++ Practice/MainForm.Designer.cs
@@ -28,204 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
-            tabPage4 = new TabPage();
-            tabPage5 = new TabPage();
-            dataGridView1 = new DataGridView();
-            dataGridView2 = new DataGridView();
-            panel1 = new Panel();
-            button1 = new Button();
-            panel3 = new Panel();
-            dataGridView4 = new DataGridView();
-            dataGridView5 = new DataGridView();
-            button2 = new Button();
-            dataGridView3 = new DataGridView();
-            panel2 = new Panel();
             button3 = new Button();
-            tabPage3 = new TabPage();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            comboBox1 = new ComboBox();
-            dateTimePicker1 = new DateTimePicker();
-            label2 = new Label();
-            tabControl1.SuspendLayout();
-            tabPage1.SuspendLayout();
-            tabPage2.SuspendLayout();
-            tabPage4.SuspendLayout();
-            tabPage5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
-            panel1.SuspendLayout();
-            panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
-            tabPage3.SuspendLayout();
+            button1 = new Button();
+            button2 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Controls.Add(tabPage3);
-            tabControl1.Controls.Add(tabPage4);
-            tabControl1.Controls.Add(tabPage5);
-            tabControl1.Dock = DockStyle.Fill;
-            tabControl1.Location = new Point(0, 0);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(800, 450);
-            tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            tabPage1.Controls.Add(dataGridView1);
-            tabPage1.Location = new Point(4, 29);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(792, 417);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "Склад";
-            tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Controls.Add(panel1);
-            tabPage2.Controls.Add(dataGridView2);
-            tabPage2.Location = new Point(4, 29);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(792, 417);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Заказы";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            tabPage4.Controls.Add(dataGridView4);
-            tabPage4.Controls.Add(panel3);
-            tabPage4.Location = new Point(4, 29);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(792, 417);
-            tabPage4.TabIndex = 3;
-            tabPage4.Text = "Продажи";
-            tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // tabPage5
-            // 
-            tabPage5.Controls.Add(dataGridView5);
-            tabPage5.Location = new Point(4, 29);
-            tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(792, 417);
-            tabPage5.TabIndex = 4;
-            tabPage5.Text = "Поставщики";
-            tabPage5.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(3, 3);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(786, 411);
-            dataGridView1.TabIndex = 0;
-            // 
-            // dataGridView2
-            // 
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Dock = DockStyle.Fill;
-            dataGridView2.Location = new Point(3, 3);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.RowHeadersWidth = 51;
-            dataGridView2.RowTemplate.Height = 29;
-            dataGridView2.Size = new Size(786, 411);
-            dataGridView2.TabIndex = 1;
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(button1);
-            panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(3, 3);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(300, 411);
-            panel1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(54, 123);
-            button1.Name = "button1";
-            button1.Size = new Size(158, 29);
-            button1.TabIndex = 1;
-            button1.Text = "Оформить заказ";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            panel3.Controls.Add(label2);
-            panel3.Controls.Add(dateTimePicker1);
-            panel3.Controls.Add(comboBox1);
-            panel3.Controls.Add(textBox1);
-            panel3.Controls.Add(label1);
-            panel3.Controls.Add(button2);
-            panel3.Dock = DockStyle.Top;
-            panel3.Location = new Point(0, 0);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(792, 125);
-            panel3.TabIndex = 0;
-            // 
-            // dataGridView4
-            // 
-            dataGridView4.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView4.Dock = DockStyle.Fill;
-            dataGridView4.Location = new Point(0, 125);
-            dataGridView4.Name = "dataGridView4";
-            dataGridView4.RowHeadersWidth = 51;
-            dataGridView4.RowTemplate.Height = 29;
-            dataGridView4.Size = new Size(792, 292);
-            dataGridView4.TabIndex = 1;
-            // 
-            // dataGridView5
-            // 
-            dataGridView5.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView5.Dock = DockStyle.Fill;
-            dataGridView5.Location = new Point(0, 0);
-            dataGridView5.Name = "dataGridView5";
-            dataGridView5.RowHeadersWidth = 51;
-            dataGridView5.RowTemplate.Height = 29;
-            dataGridView5.Size = new Size(792, 417);
-            dataGridView5.TabIndex = 0;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(576, 39);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 0;
-            button2.Text = "Продать";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView3
-            // 
-            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView3.Dock = DockStyle.Fill;
-            dataGridView3.Location = new Point(0, 0);
-            dataGridView3.Name = "dataGridView3";
-            dataGridView3.RowHeadersWidth = 51;
-            dataGridView3.RowTemplate.Height = 29;
-            dataGridView3.Size = new Size(792, 417);
-            dataGridView3.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(0, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(792, 125);
-            panel2.TabIndex = 1;
             // 
             // button3
             // 
@@ -236,105 +45,77 @@
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // button1
             // 
-            tabPage3.Controls.Add(panel2);
-            tabPage3.Controls.Add(dataGridView3);
-            tabPage3.Location = new Point(4, 29);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(792, 417);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "Поставки";
-            tabPage3.UseVisualStyleBackColor = true;
+            button1.Dock = DockStyle.Left;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 450);
+            button1.TabIndex = 0;
+            button1.Text = "Заказ";
+            button1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // button2
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(40, 17);
-            label1.Name = "label1";
-            label1.Size = new Size(51, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Товар";
+            button2.Dock = DockStyle.Top;
+            button2.Location = new Point(167, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(633, 104);
+            button2.TabIndex = 1;
+            button2.Text = "Продажи";
+            button2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // button4
             // 
-            textBox1.Location = new Point(216, 41);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(172, 27);
-            textBox1.TabIndex = 2;
+            button4.Dock = DockStyle.Right;
+            button4.Location = new Point(637, 104);
+            button4.Name = "button4";
+            button4.Size = new Size(163, 346);
+            button4.TabIndex = 2;
+            button4.Text = "Поставщики";
+            button4.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // button5
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(40, 40);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(170, 28);
-            comboBox1.TabIndex = 3;
+            button5.Dock = DockStyle.Fill;
+            button5.Location = new Point(167, 104);
+            button5.Name = "button5";
+            button5.Size = new Size(470, 346);
+            button5.TabIndex = 3;
+            button5.Text = "Склад";
+            button5.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // button6
             // 
-            dateTimePicker1.Location = new Point(394, 41);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(176, 27);
-            dateTimePicker1.TabIndex = 4;
+            button6.Dock = DockStyle.Bottom;
+            button6.Location = new Point(167, 345);
+            button6.Name = "button6";
+            button6.Size = new Size(470, 105);
+            button6.TabIndex = 4;
+            button6.Text = "Поставки";
+            button6.UseVisualStyleBackColor = true;
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(216, 18);
-            label2.Name = "label2";
-            label2.Size = new Size(90, 20);
-            label2.TabIndex = 5;
-            label2.Text = "Количество";
-            // 
-            // Form1
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(tabControl1);
-            Name = "Form1";
-            Text = "Form1";
-            tabControl1.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
-            tabPage2.ResumeLayout(false);
-            tabPage4.ResumeLayout(false);
-            tabPage5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
-            panel1.ResumeLayout(false);
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
-            tabPage3.ResumeLayout(false);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MainForm";
+            Text = "MainForm";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private DataGridView dataGridView1;
-        private TabPage tabPage2;
-        private Panel panel1;
-        private Button button1;
-        private DataGridView dataGridView2;
-        private TabPage tabPage4;
-        private DataGridView dataGridView4;
-        private Panel panel3;
-        private TabPage tabPage5;
-        private DataGridView dataGridView5;
-        private TabPage tabPage3;
-        private Panel panel2;
-        private DataGridView dataGridView3;
-        private Label label2;
-        private DateTimePicker dateTimePicker1;
-        private ComboBox comboBox1;
-        private TextBox textBox1;
-        private Label label1;
-        private Button button2;
         private Button button3;
+        private Button button1;
+        private Button button2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
